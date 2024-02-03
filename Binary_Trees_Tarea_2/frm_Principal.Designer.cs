0bd66bd5e124123c2c7c9363085660c6ec4f1397
Raul Hernandez Molina <112280077@cuc.cr>
2024-02-02 20:36:09 -0600

    Add project files.

---
+++ Binary_Trees_Tarea_2/frm_Principal.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Binary_Trees_Tarea_2
+{
+    public partial class frm_Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            ptb_matriz = new PictureBox();
+            bnt_random = new Button();
+            btn_pausa = new Button();
+            btn_iniciar = new Button();
+            tmr_1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)ptb_matriz).BeginInit();
+            SuspendLayout();
+            // 
+            // ptb_matriz
+            // 
+            ptb_matriz.Location = new Point(31, 36);
+            ptb_matriz.Name = "ptb_matriz";
+            ptb_matriz.Size = new Size(400, 400);
+            ptb_matriz.TabIndex = 1;
+            ptb_matriz.TabStop = false;
+            // 
+            // bnt_random
+            // 
+            bnt_random.Location = new Point(535, 35);
+            bnt_random.Name = "bnt_random";
+            bnt_random.Size = new Size(155, 29);
+            bnt_random.TabIndex = 1;
+            bnt_random.Text = "Random";
+            bnt_random.UseVisualStyleBackColor = true;
+            bnt_random.Click += bnt_random_Click;
+            // 
+            // btn_pausa
+            // 
+            btn_pausa.Location = new Point(535, 89);
+            btn_pausa.Name = "btn_pausa";
+            btn_pausa.Size = new Size(155, 29);
+            btn_pausa.TabIndex = 2;
+            btn_pausa.Text = "Pausa";
+            btn_pausa.UseVisualStyleBackColor = true;
+            btn_pausa.Click += btn_pausa_Click;
+            // 
+            // btn_iniciar
+            // 
+            btn_iniciar.Location = new Point(535, 143);
+            btn_iniciar.Name = "btn_iniciar";
+            btn_iniciar.Size = new Size(155, 29);
+            btn_iniciar.TabIndex = 3;
+            btn_iniciar.Text = "Iniciar";
+            btn_iniciar.UseVisualStyleBackColor = true;
+            btn_iniciar.Click += btn_iniciar_Click;
+            // 
+            // tmr_1
+            // 
+            tmr_1.Tick += tmr_1_Tick;
+            // 
+            // frm_Principal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 487);
+            Controls.Add(btn_iniciar);
+            Controls.Add(btn_pausa);
+            Controls.Add(bnt_random);
+            Controls.Add(ptb_matriz);
+            Name = "frm_Principal";
+            Text = "El juego de la vida";
+            Load += frm_Principal_Load;
+            ((System.ComponentModel.ISupportInitialize)ptb_matriz).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox ptb_matriz;
+        private Button bnt_random;
+        private Button btn_pausa;
+        private Button btn_iniciar;
+        private System.Windows.Forms.Timer tmr_1;
+    }
+}
---
+++ Binary_Trees_Tarea_2/frm_Principal.cs
@@ -0,0 +1,99 @@
+using System.Windows.Forms;
+// Importar el proyecto con las 
+using Capa_Logica;
+
+
+namespace Binary_Trees_Tarea_2
+{
+    public partial class frm_Principal : Form
+    {
+        // Objetos e instancia de clase (solo se 
+        // puede utilizar dentro de esta clase)
+        Reglas_del_Juego reglas_juego = new Reglas_del_Juego();
+        // Variables globales estaticas de la clase
+        int longitud = Reglas_del_Juego.longitud;
+        int longitudPixel = Reglas_del_Juego.longitudPixel;
+        int[,] celulas = Reglas_del_Juego.celulas; // Matriz
+
+        public frm_Principal()
+        {
+            InitializeComponent();
+        }
+
+        #region Metodos
+
+        private void frm_Principal_load()
+        {
+
+        }
+
+        private void PintarMatriz()
+        {
+            Bitmap bmp = new Bitmap(ptb_matriz.Width, ptb_matriz.Height);
+            for (int x = 0; x < longitud; x++)
+            {
+                for (int y = 0; y < longitud; y++)
+                {
+                    if (celulas[x, y] == 0)
+                        PintarPixel(bmp, x, y, Color.White);
+                    else
+                        PintarPixel(bmp, x, y, Color.Black);
+                }
+            }
+
+            ptb_matriz.Image = bmp;
+        }
+
+        private void PintarPixel(Bitmap bmp, int x, int y, Color color)
+        {
+            for (int i = 0; i < longitudPixel; i++)
+                for (int j = 0; j < longitudPixel; j++)
+                    bmp.SetPixel(i + (x * longitudPixel), j + (y * longitudPixel), color);
+        }
+
+        private void tmr_1_Tick(object sender, EventArgs e)
+        {
+            reglas_juego.JuegoDeLaVida();
+            PintarMatriz();
+        }
+
+        #endregion
+
+        #region Eventos del form
+        private void bnt_random_Click(object sender, EventArgs e)
+        {
+            // Reinicio
+            for (int i = 0; i < longitud; i++)
+                for (int j = 0; j < longitud; j++)
+                    celulas[i, j] = 0;
+
+            Random r = new Random();
+
+            // Llenado de random
+            for (int i = 0; i < longitud; i++)
+                for (int j = 0; j < longitud; j++)
+                    celulas[i, j] = r.Next(0, 2);
+
+            PintarMatriz();
+        }
+
+        private void btn_pausa_Click(object sender, EventArgs e)
+        {
+            tmr_1.Enabled = false;
+        }
+
+        private void btn_iniciar_Click(object sender, EventArgs e)
+        {
+            tmr_1.Enabled = true;
+        }
+
+        private void frm_Principal_Load(object sender, EventArgs e)
+        {
+            // Al cargar el form principal cargara el metodo automaticamente
+            PintarMatriz();
+        }
+
+        #endregion
+
+    }
+}
